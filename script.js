0e072c69c4c9d0b3e94fc1a9e444260e7a403c3e
nambautroi00 <nhatnam.090504@gmail.com>
2024-12-24 22:31:27 +0700

    add

--- script.js
+++
@@ -1,6 +0,0 @@
-function celebrate() {
-    alert("Chúc Mừng Sinh Nhật Phương! 🎉🎂");
-    document.body.style.backgroundImage = "url('https://www.w3schools.com/w3images/confetti.gif')";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundPosition = "center";
-}
